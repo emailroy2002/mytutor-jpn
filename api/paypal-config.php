538cbb75e0fc73f0ac6250898e99ab338404add3
Roy Abellana <abellana@gmail.com>
2025-03-31 19:59:01 +0800

    live update

--- api/paypal-config.php
+++ api/paypal-config.php
@@ -1,13 +1,15 @@
 <?php
-/*Live 
+/*Live */
 $clientId       = "AWRJ1e0DeWPwtv_1Bx718mtIinkPHE7eNhF-1qSUfanfyXK0Gr8Rf2GhYjBda5pJpg5JWkfCtaGLQXrA";
 $secret         = "EGASxmRicjC-wfXSsI37IZ9ssUmBC9Ms0_J39H3snQeVSJoC_s5qTHTa3Ohh1x7nkiPkaDrwscrZsbJH";
 $url  = "https://api-m.paypal.com";
-*/
+
 
 /* Sandbox*/
+/*
 $clientId       = "ATYj-7ZzewIea9u9_RULBGM3Esdwjd-4bIx0BY0BSJ43iSPzU9BLwwMXkDmwlRmDqC4mmNAR0ZjEFyEK";
 $secret         = "ECbYtQYtWhHCKtexAro9BuiosHS904UosEZusLwRGccQIxhelClwtZJ-ea4oWgs9g1pCfm9Zhl_M2uNI";
 $url            = "https://api-m.sandbox.paypal.com";
+*/
 
 ?>
